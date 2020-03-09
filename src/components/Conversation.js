2605e5750b2307901b9e4ed9cdcd8b75e127fc16
AMCD <amcdon03@mail.bbk.ac.uk>
2020-03-09 15:09:49 +0000

    changed convo

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -3,41 +3,35 @@ import React from 'react'
 function Conversation() {
   return (
     <section className='Comments'>
-      <div className='bubble sender first'>Hello</div>
-      <div className='bubble sender last'>
-        This is a CSS demo of the Messenger chat bubbles, that merge when
-        stacked together.
-      </div>
+      <div className='bubble sender first'>Hello there!</div>
+      <div className='bubble sender last'>How are you today?</div>
 
-      <div className='bubble recipient first'>Oh that's cool!</div>
-      <div className='bubble recipient last'>
-        Did you use JavaScript to perform that kind of effect?
-      </div>
+      <div className='bubble recipient first'>Oh, I'm fine.</div>
+      <div className='bubble recipient last'>And you?</div>
 
-      <div className='bubble sender first'>No, that's full CSS3!</div>
+      <div className='bubble sender first'>Yeah, I'm great!</div>
       <div className='bubble sender middle'>
-        Take a look to the 'JS' section of this Pen... it's empty!{' '}
+        I'm not. Today, I have Maths!{' '}
         <span role='img' aria-label='smiley-face'>
           😃
         </span>
       </div>
-      <div className='bubble sender last'>
-        And it's also really lightweight!
-      </div>
+      <div className='bubble sender last'>It's really hard!</div>
 
-      <div className='bubble recipient'>Dope!</div>
+      <div className='bubble recipient'>
+        No, it's not really. It just takes a bit of logic. You can do it!
+      </div>
 
       <div className='bubble sender first'>
-        Yeah, but I still didn't succeed to get rid of these stupid .first and
-        .last classes.
+        Yeah, but I give it a go though. I don't waste time in class like some
+        people.
       </div>
       <div className='bubble sender middle'>
-        The only solution I see is using JS, or a &lt;div&gt; to group elements
-        together, but I don't want to ...
+        Some students don't care about their education ...
       </div>
       <div className='bubble sender last'>
-        I think it's more transparent and easier to group .bubble elements in
-        the same parent.
+        I think it's really important that I do well so I can get a good job. Do
+        you?
       </div>
     </section>
   )
