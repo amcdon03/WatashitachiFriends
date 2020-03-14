1cc0cff77e9ded2b37fb7b667d657c1db64b6539
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-03-14 18:26:30 +0000

    minor change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import './App.css'
-import Dashboard from './components/dashboard'
-import Title from './components/title'
-import Conversation from './components/conversation'
-import SendMessageForm from './components/sendMessageForm'
+import React from "react"
+import "./App.css"
+import Dashboard from "./components/dashboard"
+import Title from "./components/title"
+import Conversation from "./components/conversation"
+import SendMessageForm from "./components/sendMessageForm"
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Dashboard />
       <Title />
       <Conversation />
