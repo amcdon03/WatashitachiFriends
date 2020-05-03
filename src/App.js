78ede06da15458d9b99ad84da86b2b2976a864bb
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-05-03 20:54:09 +0100

    Added router with 2pages: login & dashboard inside

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react"
+import {Route, Redirect, Switch} from "react=router-dom"
 import "./App.css"
 import Dashboard from "./components/dashboard"
+import Login from "./components/login"
 
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <switch>
+      <Redirect from="/" to="/login" />
+      <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
+    </switch>
   )
 }
 
