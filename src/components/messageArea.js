a6362f46a292cde6cf35cd35a528a66368a24dfc
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-05-13 21:21:21 +0100

    changed array to add mapping; added fn to iterate

--- src/components/messageArea.js
+++ src/components/messageArea.js
@@ -1,18 +1,24 @@
 import React from "react"
 
 const dummyData = [
-  { name: "angel", message: "Hey there!" },
-  { name: "andrew", message: "Hi! How's it going!" },
-  { name: "angel", message: "Oh, things are going fine!" },
-  { name: "andrew", message: "What about for you?" },
-  { name: "angel", message: "It's all good!" },
+  { user: "bubble sender", name: "angel", message: "Hey there!" },
+  { user: "bubble recipient", name: "andrew", message: "Hi! How's it going!" },
   {
+    user: "bubble sender",
+    name: "angel",
+    message: "Oh, things are going fine!",
+  },
+  { user: "bubble recipient", name: "andrew", message: "What about for you?" },
+  { user: "bubble sender", name: "angel", message: "It's all good!" },
+  {
+    user: "bubble sender",
     name: "angel",
     message: "Although, I do have Maths today! 😬",
   },
   {
+    user: "bubble recipient",
     name: "andrew",
-    message: "😬",
+    message: "Noooo 😬",
   },
 ]
 /*
@@ -23,18 +29,32 @@ if (dummData == 'angel' ? dummData == 'sender' : dummData == 'recipient');
 <div className='bubble recipient'>{props.dummyData}</div>
 */
 
+//function code
+let addMsgObject = () => {
+  dummyData.push({
+    user: "bubble sender",
+    name: "angel",
+    message: "Yeah, noooooooooooo!",
+  },)
+  console.log(dummyData)
+}
 const MessageArea = (props) => {
   //can only return 1 element inside of return
   //but can do many if only 1 outside element, eg section
   return (
     <section className="messageList">
+      {dummyData.map(function (item) {
+        return <div className={item.user}>{item.message}</div>
+      })}
+      <button onClick={addMsgObject}>add</button>
+      {/* 
       <div className="bubble sender">{dummyData[0].message}</div>
       <div className="bubble recipient">{dummyData[1].message}</div>
       <div className="bubble sender">{dummyData[2].message}</div>
       <div className="bubble recipient">{dummyData[3].message}</div>
       <div className="bubble sender">{dummyData[4].message}</div>
       <div className="bubble sender">{dummyData[5].message}</div>
-      <div className="bubble recipient">{dummyData[6].message}</div>
+      <div className="bubble recipient">{dummyData[6].message}</div> */}
     </section>
   )
 }
