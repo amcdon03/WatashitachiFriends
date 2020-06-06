7b6026c58f82e3d8564d82f78cab290313ced7e3
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-06 14:31:59 +0100

    Replaced class with function

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,21 +1,13 @@
-import React from 'react'
+import React from "react"
 import MessageList from "./messageList.js"
 import Title from "./title"
 import SendMessageForm from "./sendMessageForm"
 //import Conversation from "./messageArea.js"
 
-class Dashboard extends React.Component {
-  render() {
-    //can only return 1 main element inside of return
-    return (
-    <section className='conversation'>
-      <Title/>
-      <MessageList />
-      <SendMessageForm/>
-
-    </section>
-    )
-  }
-}
-
-export default Dashboard
+export default () => (
+  <section className="conversation">
+    <Title />
+    <MessageList />
+    <SendMessageForm />
+  </section>
+)
