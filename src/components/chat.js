f5862ef8ceaa7b86989c5d3e10d512d20d8a9bbb
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-06 16:51:02 +0100

    removed renamed component to tidy up imports

--- src/components/chat.js
+++ src/components/chat.js
@@ -2,7 +2,6 @@ import React from "react"
 import MessageList from "./messageList.js"
 import Title from "./title"
 import MessageForm from "./messageForm"
-//import Conversation from "./messageArea.js"
 
 export default () => (
   <section className="conversation">
