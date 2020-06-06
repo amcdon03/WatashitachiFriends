fdc1bfae6dbe80ebc37b8e6874b7c9dcd5b7da5c
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-06 14:24:39 +0100

    Commented the in-built unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from "react"
+//using a react testing library
+//tests the dom output
 import { render } from "@testing-library/react"
 import App from "./App"
 it("renders welcome message", () => {
+  //get the getbyText object
   const { getByText } = render(<App />)
+
+  //expect test (library method) puts outcome in document
   expect(getByText("Learn React")).toBeInTheDocument()
 })
