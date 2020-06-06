268c9a5c26a3fe62dc96f214c38ecc8b87e0fbc7
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-06 16:52:16 +0100

    added teacher dashboard component and path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"
 import "./App.css"
 import Chat from "./components/chat"
 import Login from "./components/login"
+import Dashboard from "./components/dashboard"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Redirect exact from="/" to="/login" />
         <Route path="/login" component={Login} />
         <Route path="/chat" component={Chat} />
+        <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   )
