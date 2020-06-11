213be9967ffa2edce71c8e29525e8281c0c688fe
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-11 20:27:08 +0100

    removed extraneous fn to add on new msg

--- src/components/messageForm.js
+++ src/components/messageForm.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import MessageList from './messageList.js';
 
 export default function MessageForm() {
   //useState rtns an array with 2 params: current & setter (that updates current state)
@@ -19,16 +18,9 @@ export default function MessageForm() {
     setCurrentMessage(event.target.value); //use a library to send to the server - fetch api
   };
 
-  const renderMessagesList = () => {
-    return messages.map((item) => {
-      return <div className={item.user}>{item.text}</div>;
-    });
-  };
-
   return (
     //nothing here changes value of messages
     <div className="sendForm">
-      <ul>{renderMessagesList}</ul>
       <form onSubmit={addMessage}>
         <input onChange={updateInput} type="text"></input>
         <button type="submit">Send Message</button>
