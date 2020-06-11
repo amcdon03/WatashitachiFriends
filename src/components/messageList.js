e97f6ae0049a708299dfdd799d097f6e26f1bf19
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-11 20:58:02 +0100

    converted class to function

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,70 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class MessageList extends React.Component {
-  constructor(props) {
-    super(props);
+export default function MessageList() {
+  const [messages, setMessages] = useState([
+    { user: 'bubble sender', name: 'angel', text: 'Hey there!' },
+    {
+      user: 'bubble recipient',
+      name: 'andrew',
+      text: "Hi! How's it going!",
+    },
+    {
+      user: 'bubble sender',
+      name: 'angel',
+      text: 'Oh, things are going fine!',
+    },
+    {
+      user: 'bubble recipient',
+      name: 'andrew',
+      text: 'What about for you?',
+    },
+    { user: 'bubble sender', name: 'angel', text: "It's all good!" },
+    {
+      user: 'bubble sender',
+      name: 'angel',
+      text: 'Although, I do have Maths today! 😬',
+    },
+    {
+      user: 'bubble recipient',
+      name: 'andrew',
+      text: 'Noooo 😬',
+    },
+  ]);
 
-    //boilerplate
-    this.addMsgObject = this.addMsgObject.bind(this);
-
-    this.state = {
-      messages: [
-        { user: 'bubble sender', name: 'angel', text: 'Hey there!' },
-        {
-          user: 'bubble recipient',
-          name: 'andrew',
-          text: "Hi! How's it going!",
-        },
-        {
-          user: 'bubble sender',
-          name: 'angel',
-          text: 'Oh, things are going fine!',
-        },
-        {
-          user: 'bubble recipient',
-          name: 'andrew',
-          text: 'What about for you?',
-        },
-        { user: 'bubble sender', name: 'angel', text: "It's all good!" },
-        {
-          user: 'bubble sender',
-          name: 'angel',
-          text: 'Although, I do have Maths today! 😬',
-        },
-        {
-          user: 'bubble recipient',
-          name: 'andrew',
-          text: 'Noooo 😬',
-        },
-      ],
-    };
-  }
-
-  addMsgObject = () => {
+  const addMsgObject = () => {
     // add function code here
-    const { messages } = this.state;
-    this.setState({
-      messages: [
-        ...messages,
-        {
-          user: 'bubble sender',
-          name: 'angel',
-          text: 'Exactly! Oh No!',
-        },
-      ],
-    });
+    //const { messages } = state;
+    setMessages([
+      ...messages,
+      {
+        user: 'bubble sender',
+        name: 'angel',
+        text: 'Exactly! Oh No!',
+      },
+    ]);
     //console.log(this.messages)
   };
 
-  render() {
-    return (
-      <section className="messageArea">
-        {this.state.messages.map(function (item) {
-          return <div className={item.user}>{item.text}</div>;
-        })}
-        <button onClick={this.addMsgObject}>add</button>
-      </section>
-    );
-  }
+  return (
+    <section className="messageArea">
+      {messages.map(function (item) {
+        return <div className={item.user}>{item.text}</div>;
+      })}
+      <button onClick={addMsgObject}>add</button>
+    </section>
+  );
 }
-export default MessageList;
