2843ab41aed7841c379727e600b721f8c8c1f4e0
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-18 20:17:29 +0100

    msg data hoisted; components passed props

--- src/components/chatPage.js
+++ src/components/chatPage.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MessageList from './messageList.js';
 import Title from './title';
 import MessageForm from './messageForm.js';
 
 export default function ChatPage() {
+  const [messages, setMessages] = useState([
+    { user: 'bubble sender', name: 'angel', text: 'Hey there!' },
+    {
+      user: 'bubble recipient',
+      name: 'andrew',
+      text: "Hi! How's it going!",
+    },
+    {
+      user: 'bubble sender',
+      name: 'angel',
+      text: 'Oh, things are going fine!',
+    },
+    {
+      user: 'bubble recipient',
+      name: 'andrew',
+      text: 'What about for you?',
+    },
+    { user: 'bubble sender', name: 'angel', text: "It's all good!" },
+    {
+      user: 'bubble sender',
+      name: 'angel',
+      text: 'Although, I do have Maths today! 😬',
+    },
+    {
+      user: 'bubble recipient',
+      name: 'andrew',
+      text: 'Noooo 😬',
+    },
+  ]);
+
+  const sendMessage = (message) => {
+    //call setMessage and pass on value enclosed
+    setMessages([
+      ...messages,
+      {
+        user: 'bubble sender',
+        name: 'angel',
+        text: message,
+      },
+    ]); //use a library to send to the server - fetch api
+  };
+
   return (
     <section className="conversation">
       <Title />
-      <MessageList />
-      <MessageForm />
+      <MessageList messages={messages} />
+      <MessageForm onSend={sendMessage} />
     </section>
   );
 }
