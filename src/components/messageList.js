23f1c345990f725d58bffd19cf5255fa017707c9
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-18 20:32:10 +0100

    rmv btn to add msgs; transfer msg data to parent

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,56 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-export default function MessageList() {
-  const [messages, setMessages] = useState([
-    { user: 'bubble sender', name: 'angel', text: 'Hey there!' },
-    {
-      user: 'bubble recipient',
-      name: 'andrew',
-      text: "Hi! How's it going!",
-    },
-    {
-      user: 'bubble sender',
-      name: 'angel',
-      text: 'Oh, things are going fine!',
-    },
-    {
-      user: 'bubble recipient',
-      name: 'andrew',
-      text: 'What about for you?',
-    },
-    { user: 'bubble sender', name: 'angel', text: "It's all good!" },
-    {
-      user: 'bubble sender',
-      name: 'angel',
-      text: 'Although, I do have Maths today! 😬',
-    },
-    {
-      user: 'bubble recipient',
-      name: 'andrew',
-      text: 'Noooo 😬',
-    },
-  ]);
-
-  const addMsgObject = () => {
-    // add function code here
-    //const { messages } = state;
-    setMessages([
-      ...messages,
-      {
-        user: 'bubble sender',
-        name: 'angel',
-        text: 'Exactly! Oh No!',
-      },
-    ]);
-    //console.log(this.messages)
-  };
-
+export default function MessageList({ messages }) {
   return (
     <section className="messageArea">
       {messages.map(function (item) {
         return <div className={item.user}>{item.text}</div>;
       })}
-      <button onClick={addMsgObject}>add</button>
     </section>
   );
 }
