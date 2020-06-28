8777e09c431c7cc99dad0fac3f8a011f9b1aa327
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-28 23:45:21 +0100

    refactored App.js w/new page components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
-import ChatPage from './components/chatPage';
-import Login from './components/login';
-import Dashboard from './components/dashboard';
+import Home from './pages/home';
+import Chat from './pages/chat';
+import Login from './pages/login';
+// import Signup from './pages/signup';
+import Dashboard from './pages/dashboard';
+//import { auth } from './services/firebase';
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Redirect exact from="/" to="/login" />
+        <Redirect exact from="/" to={Home} />
         <Route path="/login" component={Login} />
-        <Route path="/chatPage" component={ChatPage} />
+        {/* <Route path="/signup" authenticated={this.state.authenticated} component={Signup} /> */}
+        <Route path="/chat" component={Chat} />
         <Route path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
 }
-
-export default App;
