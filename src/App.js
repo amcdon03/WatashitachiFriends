1f93de3e657c779daee16410834506a43838fb5d
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-06-30 18:27:31 +0100

    created login-signup redirection functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React /* useState */ from 'react';
+import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Chat from './pages/chat';
 import Login from './pages/login';
-// import Signup from './pages/signup';
+import Signup from './pages/signup';
 import Dashboard from './pages/dashboard';
 //import { auth } from './services/firebase';
+import { PublicRoute, PrivateRoute } from './utils/route';
 
 export default function App() {
+  //const [authenticated, setAuthenticated] = useState(false);
+
   return (
     <BrowserRouter>
       <Switch>
         <Redirect exact from="/" to={Home} />
-        <Route path="/login" component={Login} />
-        {/* <Route path="/signup" authenticated={this.state.authenticated} component={Signup} /> */}
-        <Route path="/chat" component={Chat} />
-        <Route path="/dashboard" component={Dashboard} />
+        <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login} />
+        <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup} />
+        <PrivateRoute path="/chat" authenticated={this.state.authenticated} component={Chat} />
+        <PrivateRoute
+          path="/dashboard"
+          authenticated={this.state.authenticated}
+          component={Dashboard}
+        />
       </Switch>
     </BrowserRouter>
   );
