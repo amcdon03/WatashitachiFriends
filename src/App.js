e851cf01be500c2851d03294bd462c0ee1eed324
amcdon03 <amcdon03@mail.bbk.ac.uk>
2020-07-01 21:49:10 +0100

    followed class- component tutorial login/setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import React /* useState */ from 'react';
-import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Chat from './pages/chat';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Dashboard from './pages/dashboard';
-//import { auth } from './services/firebase';
+import { auth } from './services/firebase';
 import { PublicRoute, PrivateRoute } from './utils/route';
 
-export default function App() {
-  //const [authenticated, setAuthenticated] = useState(false);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true,
+    };
+  }
 
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Redirect exact from="/" to={Home} />
-        <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login} />
-        <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup} />
-        <PrivateRoute path="/chat" authenticated={this.state.authenticated} component={Chat} />
-        <PrivateRoute
-          path="/dashboard"
-          authenticated={this.state.authenticated}
-          component={Dashboard}
-        />
-      </Switch>
-    </BrowserRouter>
-  );
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        });
+      }
+    });
+  }
+
+  render() {
+    return this.state.loading === true ? (
+      <h2>Loading...</h2>
+    ) : (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login} />
+          <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup} />
+          <PrivateRoute path="/chat" authenticated={this.state.authenticated} component={Chat} />
+          <PrivateRoute
+            path="/dashboard"
+            authenticated={this.state.authenticated}
+            component={Dashboard}
+          />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default App;
