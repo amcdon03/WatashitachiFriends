066857759a8fe005348bf47e3f9d043d70d5a424
kandy Tpad <kandytpad@pop-os.localdomain>
2020-07-07 21:13:53 +0100

    refactored w/declared fn not anon fn for onChange

--- src/components/messageForm.js
+++ src/components/messageForm.js
@@ -5,8 +5,14 @@ export default function MessageForm({ onSend }) {
   //tracks state of the input box
   const [currentMessage, setCurrentMessage] = useState('');
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSend(currentMessage); //passed up to the parent
+    //trigger DOM refresh by setting new value
+    setCurrentMessage('');
+  }
   //this fn takes in an event
-  const updateMessage = (event) => {
+  const handleMessageChange = (event) => {
     //set the state, above, by passing in its fn elem
     //call setCurrentMessage and pass on value enclosed
     setCurrentMessage(event.target.value); //use a library to send to the server - fetch api
@@ -15,20 +21,15 @@ export default function MessageForm({ onSend }) {
   return (
     //nothing here changes value of messages
     <div className="sendForm">
-      {/*handler capturing onSubmit event, calling onSend on line 26 and passing currentMessage*/}
+      {/*handler capturing onSubmit event, ie submit action, calling onSend and passing it currentMessage*/}
       <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          onSend(currentMessage); //passed up to the parent
-          //trigger DOM refresh by setting new value
-          setCurrentMessage('');
-        }}
+        onSubmit={handleSubmit}
       >
-        {/*input has 3 props, inc onChange = user typing*/}
+        {/*input has props, inc onChange = user typing*/}
         <input
           name="myMessage"
-          //onChange handler fires method called updateMessage
-          onChange={updateMessage}
+          //onChange handler fires method called handleMessageChange
+          onChange={handleMessageChange}
           type="text"
           value={currentMessage}
           placeholder="Your message"
