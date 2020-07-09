7eb1a5bcdf0a00ae5ff25cf8ff4862eb81d80f5c
Kandy Tpad <kandytpad@pop-os.localdomain>
2020-07-09 22:34:58 +0100

    changed header

--- src/components/title.js
+++ src/components/title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
   return (
     <section className="headerArea">
-      <h3>Let's Chat!</h3>
+      <h3>OK Let's Chat!</h3>
     </section>
   );
 }
