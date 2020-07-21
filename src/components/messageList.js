320158b3ab28d128a295a984dda2193e6b06a2b3
Kandy TP <kandytp@pop-os.localdomain>
2020-07-21 21:49:13 +0100

    minor: used arrow fn

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export default function MessageList({ messages }) {
   return (
     <section className="messageArea">
-      {messages.map(function (item) {
+      {messages.map((item) => {
         return <div className={item.user}>{item.text}</div>;
+        // return <div className={item.user}>{item.content}</div>;
       })}
     </section>
   );
