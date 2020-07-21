b641492502e5614901cee130cc0ee9788d6393b2
Kandy TP <kandytp@pop-os.localdomain>
2020-07-21 21:48:43 +0100

    removed comments & prettier'd

--- src/components/messageForm.js
+++ src/components/messageForm.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function MessageForm({ onSend }) {
-  //useState rtns an array with 2 params: current & setter (a fn that updates current state)
-  //tracks state of the input box
-  const [currentMessage, setCurrentMessage] = useState('');
+  const [currentMessage, setCurrentMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSend(currentMessage); //passed up to the parent
-    //trigger DOM refresh by setting new value
-    setCurrentMessage('');
-  }
-  //this fn takes in an event
+    onSend(currentMessage);
+    setCurrentMessage("");
+  };
+
   const handleMessageChange = (event) => {
-    //set the state, above, by passing in its fn elem
-    //call setCurrentMessage and pass on value enclosed
-    setCurrentMessage(event.target.value); //use a library to send to the server - fetch api
+    setCurrentMessage(event.target.value);
   };
 
   return (
     //nothing here changes value of messages
     <div className="sendForm">
       {/*handler capturing onSubmit event, ie submit action, calling onSend and passing it currentMessage*/}
-      <form
-        onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit}>
         {/*input has props, inc onChange = user typing*/}
         <input
           name="myMessage"
@@ -34,6 +27,7 @@ export default function MessageForm({ onSend }) {
           value={currentMessage}
           placeholder="Your message"
         ></input>
+        {/* {error ? <p>{writeError}</p> : null} */}
         <button type="submit">Send</button>
       </form>
     </div>
