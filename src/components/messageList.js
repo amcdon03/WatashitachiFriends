9a2dc7ae804247807a12606589b936a9d26da974
Kandy TP <kandytp@pop-os.localdomain>
2020-07-30 20:30:59 +0100

    added steps to set CSS values

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 export default function MessageList({ messages }) {
+  /*
+    1. Pass the current user ID to the MessageList component
+       (this component) as a prop.
+    2. Every message (in messages) will have a "uid"
+       property which contains the ID of the user who sent
+       that message.
+    3. Compare the current user ID with the "uid"
+       on a message (e.g. currentUserId == item.uid) and
+       apply a different CSS class if they are the same.
+  */
   return (
     <section className="messageArea">
       {messages.map((item) => {
-        return <div className={item.user}>{item.text}</div>;
-        // return <div className={item.user}>{item.content}</div>;
+        return <div className={item.uid}>{item.content}</div>;
       })}
     </section>
   );
