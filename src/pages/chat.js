8da8694b1ea609c5d151f7b19904d26520b61756
Kandy TP <kandytp@pop-os.localdomain>
2020-08-13 21:33:11 +0100

    fixed unread vars, found msgID, added passnotes

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -5,24 +5,41 @@ import MessageList from "../components/messageList.js";
 import Title from "../components/title";
 import MessageForm from "../components/messageForm.js";
 
+/*1. create a new project in current state: git.clone
+ or commit, then undo commit
+FIXED CSS
+- had to identify/pull out unique message key (set by FB), needed by React
+- used a filter on classname
+- error: I passed a  typo as currentUserID != currentUserId
+PROPS
+- made sure prop name here matched prop name passed to child (the 'props' bit is like the 'property' bit in CSS)
+TESTS
+- error msg in msgForm worked by passing value 'test' instead of 'writeError'
+- console logged snapshot, snapshot.val to find msg uid
+- console logged msg1 to see msgs in snapshot
+- used Inspector to see if className rendered as desired in msgLst (mistake, wrote: bubble.sender, not bubble sender)
+*/
+
 export default function Chat() {
-  const [user, setUser] = useState(auth().currentUser);
+  // useState -> {user, setUser} => const result = useState(,,,); const user = result.user ; const setUser = result.setUser;
+  const [user] = useState(auth().currentUser);
   const [chats, setChats] = useState([]);
   const [readError, setReadError] = useState(null);
   const [writeError, setWriteError] = useState(null);
 
+  //This Fn uses a callback - consisting of Anonymous Fn and empty array
   useEffect(() => {
-    setReadError(null);
     try {
       db.ref("chats").on("value", (snapshot) => {
         let messages = [];
         //console.log("messages1", messages);
         snapshot.forEach((snap) => {
-          messages.push(snap.val());
-          //console.log("snapshot", snapshot);
+          messages.push({ key: snap.key, ...snap.val() });
+          //console.log("snap", snap.val);
         });
         setChats(messages);
-      }); //console.log("messages2", messages);
+        //console.log("messages2", messages);
+      });
     } catch (error) {
       setReadError(error.message);
       //console.log("error", error);
@@ -46,12 +63,13 @@ export default function Chat() {
 
   return (
     <section className="conversation">
+      {readError ? readError : ""}
       <div>
         Logged in as <strong>{user.email}</strong>
       </div>
       <Title />
       {/* trying to create a prop to hold the current user's id */}
-      <MessageList messages={chats} currentUserID={user.uid} />
+      <MessageList messages={chats} currentUserId={user.uid} />
       <MessageForm onSend={sendMessage} error={writeError} />
     </section>
   );
