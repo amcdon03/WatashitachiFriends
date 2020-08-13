a72210e86eef9086e974a5e93cba0cc56cb512d7
Kandy TP <kandytp@pop-os.localdomain>
2020-08-13 21:23:29 +0100

    fixed bubble css based on task list (now deleted)

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -1,21 +1,23 @@
 import React from "react";
 
 export default function MessageList({ messages, currentUserId }) {
-  /*
-    1. Pass the current user ID to the MessageList component
-       (this component) as a prop.
-    2. Every message (in messages) has a "uid"
-       property which contains the ID of the user who sent
-       that message.
-    3. Compare the current user ID with the "uid"
-       on a message (e.g. currentUserId == item.uid) and
-       apply bubble.sender CSS class if they are the same;
-       bubble.recipient CSS if they are different.
-  */
   return (
     <section className="messageArea">
-      {messages.map((item) => {
-        return <div className={item.uid}>{item.content}</div>;
+      {messages.map((message) => {
+        return (
+          // div has 2 elements: className and key
+          // msg is displayed in between
+          <div
+            className={
+              currentUserId === message.uid
+                ? "bubble sender"
+                : "bubble recipient"
+            }
+            key={message.key}
+          >
+            {message.content}
+          </div>
+        );
       })}
     </section>
   );
