232bc25a1e5df345125c3dc25f48ba95ad896bcb
Kandy TP <kandytp@pop-os.localdomain>
2020-09-06 14:55:43 +0100

    refactored class component w/hooks

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,70 +1,82 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SignIn } from "../utils/auth";
 
-export default class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      email: "",
-      password: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+export default function Login() {
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  handleChange(event) {
-    this.setState({
+  /*     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }; */
+
+  /*   handleChange(event) {
+        this.setState({
+          [event.target.name]: event.target.value,
+        });
+      }; */
+
+  const handleChange = (event) => {
+    (setEmail, setPassword)({
       [event.target.name]: event.target.value,
     });
-  }
+  };
 
-  async handleSubmit(event) {
+  /*   async handleSubmit(event) {
+        event.preventDefault();
+        setError({ error: "" });
+        try {
+          await SignIn(email, password);
+          //console.log('User signs in')
+        } catch (error) {
+          setError({ error: error.message });
+        }
+      } */
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.setState({ error: "" });
+    setError({ error: "" });
     try {
-      await SignIn(this.state.email, this.state.password);
+      SignIn(email, password);
       //console.log('User signs in')
     } catch (error) {
-      this.setState({ error: error.message });
+      setError({ error: error.message });
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="conversation">
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <h1>Log in to Watashitachi Friends</h1>
-          <p>Fill in the form below to login to your account.</p>
-          <div>
-            <input
-              placeholder="School Email"
-              name="email"
-              type="email"
-              onChange={this.handleChange}
-              value={this.state.email}
-            />
-          </div>
-          <div>
-            <input
-              placeholder="Password"
-              name="password"
-              onChange={this.handleChange}
-              value={this.state.password}
-              type="password"
-            />
-          </div>
-          <div>
-            {this.state.error ? <p>{this.state.error}</p> : null}
-            <button type="submit">Login</button>
-          </div>
-          <hr />
-          <p>
-            Don't have an account? <Link to="/Register">Get registered</Link>
-          </p>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="conversation">
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <h1>Log in to Watashitachi Friends</h1>
+        <p>Fill in the form below to login to your account.</p>
+        <div>
+          <input
+            placeholder="School Email"
+            name="email"
+            type="email"
+            onChange={handleChange}
+            value={email}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            value={password}
+            type="password"
+          />
+        </div>
+        <div>
+          {error ? <p>{error}</p> : null}
+          <button type="submit">Login</button>
+        </div>
+        <hr />
+        <p>
+          Don't have an account? <Link to="/Register">Get registered</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
