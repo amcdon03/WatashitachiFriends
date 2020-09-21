c0721b182c394c51e7653dd7e7d7f5a8dcea8680
Kandy TP <kandytp@pop-os.localdomain>
2020-09-21 17:41:41 +0100

    minor: removed unused settings const

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,5 @@
 import firebase from "firebase";
 
-const settings = { timestampsInSnapshots: true };
-
 var firebaseConfig = {
   apiKey: "AIzaSyCCSspPlcPvQkIIdcR7pCaWxkADZd7Lw6Y",
   authDomain: "watashinofriends.firebaseapp.com",
