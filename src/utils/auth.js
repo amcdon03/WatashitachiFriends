96a485c473247d21dbf156d84070f75060642018
Kandy TP <kandytp@pop-os.localdomain>
2020-09-21 17:38:36 +0100

    added notes on updating user profile

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,10 +4,39 @@ export function SignUp(email, password) {
   return auth().createUserWithEmailAndPassword(email, password);
 }
 
+///////////////////////////////////////////////////
 export function SignIn(email, password) {
   return auth().signInWithEmailAndPassword(email, password);
 }
+
+export default function Profile() {
+  // 1. How to get a user's info to complete a profile
+  return auth().currentUser.displayName;
+}
+
 ///////////////////////////////////////////////////
+// 2. https://firebase.google.com/docs/auth/web/start
+/* Attach the observer using the onAuthStateChanged method.
+  When a user successfully signs in, you can get information 
+  about the user in the observer.
+  
+  auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    const displayName = user.displayName;
+    const email = user.email;
+    const emailVerified = user.emailVerified;
+    const photoURL = user.photoURL;
+    const isAnonymous = user.isAnonymous;
+    const uid = user.uid;
+    const providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+}); */
+
 export function SchoolName(schoolName) {
   const user = auth().currentUser;
 
