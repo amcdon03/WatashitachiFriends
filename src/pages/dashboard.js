890cec8a85d86e0d990d51b2dbffffe9c460d99b
Kandy TP <kandytp@pop-os.localdomain>
2020-09-26 19:19:01 +0100

    made tch dash available, but have no route to dash

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,4 @@
-/* import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../services/firebase";
 import { db } from "../services/firebase";
 import Header from "../components/Header";
@@ -13,28 +13,29 @@ export default function Dashboard() {
 
   useEffect(() => {
     try {
-      db.ref("messages/ + messagesID + toApprove/ + toApproveID + {user}}/").on(
-        "value",
-        (snapshot) => {
-          // collect all messages that're pending approval
-          let messagesForApprovalDashboard = [];
-          snapshot.forEach((snap) => {
-            messagesForApprovalDashboard.push({ key: snap.key, ...snap.val() });
-            //console.log("snap", snap.val);
-          });
-          // filter messages only for teacher's own students
-          const messageForTheTeacher = db.ref("messages").filter((message) => {
+      // create nested endpoint in the db
+      db.ref("messages/toApprove/ ").on("value", (snapshot) => {
+        // collect all messages that're pending approval
+        let messagesForApprovalDashboard = [];
+        console.log("msg4tchr:", messagesForApprovalDashboard);
+        snapshot.forEach((snap) => {
+          messagesForApprovalDashboard.push({ key: snap.key, ...snap.val() });
+          //console.log("snap", snap.val);
+        });
+        // filter messages only for teacher's own students
+        const messageForTheTeacher = db
+          .ref("messages/toDisplay")
+          .filter((messages) => {
             return messageForTheTeacher;
           });
-          if (messageForTheTeacher.teacher === user)
-            setDashboard(messagesForApprovalDashboard);
-        }
-      );
+        if (messageForTheTeacher.teacher === user)
+          setDashboard(messagesForApprovalDashboard);
+      });
     } catch (error) {
       setReadError(error.message);
       // console.log("error", error);
     }
-  }, [user]);
+  }, []);
 
   return (
     <section className="conversation">
@@ -46,4 +47,3 @@ export default function Dashboard() {
     </section>
   );
 }
- */
